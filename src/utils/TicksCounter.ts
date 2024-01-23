d1f0e47e637fa8b5c71deac9b23f9db62f58d4ea
Rayana Shiba <rayana@rayana.trade>
2024-01-23 10:03:39 +0100

    fast ticks counter implementation

---
+++ src/utils/TicksCounter.ts
@@ -0,0 +1,65 @@
+type Entry = {
+  [key: string]: {
+    total: number;
+    buys: number;
+    sells: number;
+  };
+};
+
+const EMPTY_ENTRY: Entry = {};
+
+class TicksCounter {
+  private ticksToStore: number;
+  private lastSixtySecondsTicks: Entry[] = [];
+  private currentSecondTicks: Entry = {
+    ...EMPTY_ENTRY,
+  };
+
+  constructor(ticksToStore: number = 60) {
+    this.ticksToStore = ticksToStore;
+
+    setInterval(() => {
+      this.recordCurrentSecond();
+    }, 1_000);
+
+    this.recordCurrentSecond();
+  }
+
+  recordCurrentSecond() {
+    this.lastSixtySecondsTicks.push({ ...this.currentSecondTicks });
+    this.currentSecondTicks = {
+      ...EMPTY_ENTRY,
+    };
+    if (this.lastSixtySecondsTicks.length > this.ticksToStore) {
+      this.lastSixtySecondsTicks.shift();
+    }
+  }
+
+  getLastN(seconds: number): Entry {
+    const stats: Entry = {};
+    let startPointTotal = this.lastSixtySecondsTicks.length - seconds;
+    if (this.lastSixtySecondsTicks.length < seconds) {
+      startPointTotal = 0;
+    }
+    for (let i = startPointTotal; i < this.lastSixtySecondsTicks.length; i++) {
+      for (const [symbol, trades] of Object.entries(this.lastSixtySecondsTicks[i])) {
+        if (!stats[symbol]) {
+          stats[symbol] = {
+            total: 0,
+            buys: 0,
+            sells: 0,
+          };
+        }
+        stats[symbol].total += trades.total;
+        stats[symbol].buys += trades.buys;
+        stats[symbol].sells += trades.sells;
+      }
+    }
+    return stats;
+  }
+
+  getTopN(seconds: number, top: number) {
+    const result = Object.entries(this.getLastN(seconds));
+    return result.sort((a, b) => b[1].total - a[1].total).slice(0, top);
+  }
+}
