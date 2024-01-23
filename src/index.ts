4e6752ea69010da105fed9cb5b1fabd43ff713bb
Rayana Shiba <rayana@rayana.trade>
2024-01-23 10:03:39 +0100

    implement basic yarn dev command

---
+++ src/index.ts
@@ -0,0 +1,28 @@
+import { createHermesService } from '@rayanacapital/hermes';
+
+const pkg = require('../package.json');
+
+const port = process.env.PORT ? +process.env.PORT : 8087;
+
+const service = createHermesService({
+  // ...
+});
+
+async function start() {
+  await service.start(port);
+
+  console.log(`screener-backend server v${pkg.version} is running`);
+  console.log(`HTTP port: ${port}`);
+  console.log(`WS port: ${port + 1}`);
+}
+
+start();
+
+process
+  .on('unhandledRejection', (reason, p) => {
+    console.error('Unhandled Rejection at Promise', reason, p);
+  })
+  .on('uncaughtException', (err) => {
+    console.error('Uncaught Exception thrown', err);
+    process.exit(1);
+  });
